24756b5a4d3489397f8831598fe30e581603bb7f
pradheep <padhupradheep@gmail.com>
2019-09-12 10:47:31 +0200

    movebase commands runs in a loop

--- mpo_500_moveit/src/mpo_500_cycle_time_node.py
+++ mpo_500_moveit/src/mpo_500_cycle_time_node.py
@@ -3,30 +3,34 @@ import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-def movebase_client():
+def movebase_client(x,y,w):
 
-    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
-    client.wait_for_server()
+	client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+	client.wait_for_server()
 
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "map"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = 0.5
-    goal.target_pose.pose.orientation.w = 1.0
+	goal = MoveBaseGoal()
+	goal.target_pose.header.frame_id = "map"
+	goal.target_pose.header.stamp = rospy.Time.now()
+	goal.target_pose.pose.position.x = x
+	goal.target_pose.pose.position.x = y
+	goal.target_pose.pose.orientation.w = w
 
-    client.send_goal(goal)
-    wait = client.wait_for_result()
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-        return client.get_result()
+	client.send_goal(goal)
+	wait = client.wait_for_result()
+	if not wait:
+		rospy.logerr("Action server not available!")
+		rospy.signal_shutdown("Action server not available!")
+	else:
+		return client.get_result()
+
+def main():
+	rospy.init_node('movebase_client_py')
+	while not rospy.is_shutdown():
+		result = movebase_client(0,2.5,2.5)
+		result1 = movebase_client(0,0,2.5)
+
 
 if __name__ == '__main__':
-    try:
-        rospy.init_node('movebase_client_py')
-        result = movebase_client()
-        if result:
-            rospy.loginfo("Goal execution done!")
-    except rospy.ROSInterruptException:
-        rospy.loginfo("Navigation test finished.")
+	main()
+
+	

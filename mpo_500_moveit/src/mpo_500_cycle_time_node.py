206be141679daf9808b79d5cb1fc0a928c997c29
pradheep <padhupradheep@gmail.com>
2019-09-12 13:01:15 +0200

    minor changes

--- mpo_500_moveit/src/mpo_500_cycle_time_node.py
+++ mpo_500_moveit/src/mpo_500_cycle_time_node.py
@@ -3,7 +3,7 @@ import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-def movebase_client(x,y,w):
+def movebase_client(x,y,z,w):
 
 	client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
 	client.wait_for_server()
@@ -12,7 +12,8 @@ def movebase_client(x,y,w):
 	goal.target_pose.header.frame_id = "map"
 	goal.target_pose.header.stamp = rospy.Time.now()
 	goal.target_pose.pose.position.x = x
-	goal.target_pose.pose.position.x = y
+	goal.target_pose.pose.position.y = y
+	goal.target_pose.pose.orientation.z = z
 	goal.target_pose.pose.orientation.w = w
 
 	client.send_goal(goal)
@@ -26,8 +27,8 @@ def movebase_client(x,y,w):
 def main():
 	rospy.init_node('movebase_client_py')
 	while not rospy.is_shutdown():
-		result = movebase_client(0,2.5,2.5)
-		result1 = movebase_client(0,0,2.5)
+		result = movebase_client(0,0,0,0.999)
+		result1 = movebase_client(2,0,0,0.999)
 
 
 if __name__ == '__main__':
